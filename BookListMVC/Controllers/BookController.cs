f1622992ee97adbd919c2535ddd0fdbfa6ddb791
Abi <Abi>
2021-02-15 10:27:34 +0545

    book list to show

--- BookListMVC/Controllers/BookController.cs
+++ BookListMVC/Controllers/BookController.cs
@@ -2,15 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookListMVC.Controllers
 {
     public class BookController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Book Book { get; set; }
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult Upsert(int? id)
+        {
+            Book = new Book();
+            if (id == null)
+            {
+                //create
+                return View(Book);
+            }
+            //update
+            Book = _db.Books.FirstOrDefault(u => u.Id == id);
+            if (Book == null)
+            {
+                return NotFound();
+            }
+            return View(Book);
+        }
+
+
     }
 }
